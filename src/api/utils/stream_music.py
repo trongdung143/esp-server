c76baee2459509380e1f06e571e28b225c548f68
trongdung07004 <trongdung143@gmail.com>
2025-12-03 21:32:53 +0700

    fx

--- src/api/utils/stream_music.py
+++ src/api/utils/stream_music.py
@@ -12,7 +12,6 @@ async def stream_music(file_path: str, chunk_size: int = 2024):
         with open(file_path, "rb") as f:
             while chunk := await asyncio.to_thread(f.read, chunk_size):
                 yield chunk
-                print("ok")
     except RuntimeError as e:
         pass
     finally:
